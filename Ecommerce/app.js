3a14677c58b5e124a3c770fcdd4ff34d0cb965c8
ayoubha <ayoub.hammani@e-campus.uab.cat>
2022-11-23 02:18:53 +0100

    alpha r0

--- Ecommerce/app.js
+++ Ecommerce/app.js
@@ -23,12 +23,6 @@ app.set('views', path.join(__dirname, ('views')));
 app.use(express.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
-
-
-
-
-
-
 app.get('/', home)
 
 app.get('/login',login)
