d8182d703272f1350243113741f66cb1b0d72150
ayoubha <ayoub.hammani@e-campus.uab.cat>
2022-11-24 17:14:58 +0100

    Responsive improvements

--- Portfolio/index.js
+++ Portfolio/index.js
@@ -4,17 +4,15 @@ const path = require('path');
 const errorsRoute = require('./routes/Errors')
 const headPage =require('./routes/headpage');
 const contact =require('./routes/contact');
-
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, ('/views')));
+app.use('/contact', contact);
 app.use(express.static(path.join(__dirname,'/public')));
 app.use(express.json());
-
 app.use(express.urlencoded({extended:true}));
 
-app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, ('/views')));
 
-app.use('/contact', contact);
 
 app.use('/', headPage);
 
