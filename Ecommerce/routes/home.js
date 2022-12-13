3ce89f29756718cb8dac66ac69b7aedae2bb82c5
ayoubha <ayoub.hammani@e-campus.uab.cat>
2022-12-13 15:58:44 +0100

    REDESING

--- Ecommerce/routes/home.js
+++ Ecommerce/routes/home.js
@@ -8,14 +8,18 @@ const Store = require('../models/store');
 
 router.get('',  (req,res)=>{
     console.log(req.user)
-    res.render('home/home')
+    res.render('home/homeTest')
 })
 router.get('/search', (req,res)=>{
-    res.send('heyyy');
+    res.send('No results found!');
 })
 router.post("/search", async (req,res) => {
     const allProduct = await Product.find({ name: { $regex: req.body.q, $options: 'i'}})
     res.render('products/searchResult', {allProduct})
+})
+router.get('/search/page2', async(req,res)=>{
+    const allProduct = await Product.find({ name: { $regex: String(req.body.q), $options: 'i'}})
+    res.render('products/nextPage' , {allProduct})
 })
 
 
