2e3974f8855cfd9ce45f55862044525014cfbeaf
ayoubha <ayoub.hammani@e-campus.uab.cat>
2022-12-18 22:27:03 +0100

    Ajustes

--- Ecommerce2/routes/requireLogin.js
+++ Ecommerce2/routes/requireLogin.js
@@ -2,6 +2,8 @@ const requireLogin = (req,res,next)=>{
     console.log(req.user)
     if(!req.isAuthenticated()){
         return res.render('needTo/needTo')
+    }else{
+        return res.render('stores/new')
     }
     next();
 }
